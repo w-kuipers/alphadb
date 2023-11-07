45f26b48e9b048c9e51c94fcc81e64a068f12ac1
Wibo Kuipers <w.kuipers@filmage.nl>
2023-11-07 20:00:56 +0100

    Fixed: Typo in SourceVerification classname

--- tests/test_version_verification.py
+++ tests/test_version_verification.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from alphadb.version_verification import SourceVeficication
+from alphadb.version_verification import SourceVerification
 
 #### SourceVeficication will first check for _id, then for name
 def test_no_version_number_and_name():
@@ -37,17 +37,17 @@ def test_no_version_number_and_name():
 
 
 
-    verification_no_version = SourceVeficication(version_source_no_version)
+    verification_no_version = SourceVerification(version_source_no_version)
     assert verification_no_version.verify() == [("LOW", "This version source does not contain any versions")]
     
-    verification_no_name = SourceVeficication(version_source_no_name)
+    verification_no_name = SourceVerification(version_source_no_name)
     assert verification_no_name.verify() == [("CRITICAL", "No rootlevel name was specified"), ("CRITICAL", "Version index 0: Missing a version number")]
 
 def test_empty_createtable_altertable():
     
     version_empty_createtable_altertable = {}
 
-    verification_empty_createtable = SourceVeficication({})
+    verification_empty_createtable = SourceVerification({})
     verification_empty_createtable.createtable(version_empty_createtable_altertable)
     verification_empty_createtable.altertable(version_empty_createtable_altertable)
 
@@ -62,11 +62,24 @@ def test_createtable_no_type():
         }
     }
 
-    verification_createtable_no_type = SourceVeficication({})
+    verification_createtable_no_type = SourceVerification({})
     verification_createtable_no_type.createtable(version_createtable_no_type)
 
     assert verification_createtable_no_type.issues == [("CRITICAL", "Unknown version -> createtable -> table:test_table -> column:test_column: Does not contain a column type")]
 
+def test_bad_primarykey():
+    version_bad_primarykey = {
+        "test_table":{
+            "primary_key": "none",
+            "test_column": {
+                "type": "VARCHAR",
+                "length": 10
+            }
+        }
+    }
+
+    verification_bad_primarykey = SourceVerification({})
+    verification_bad_primarykey.createtable(version_bad_primarykey)
 
 def test_column_incompatibility():
     version_null_and_ai = {
@@ -75,7 +88,7 @@ def test_column_incompatibility():
         "a_i": True
     }
 
-    verification_null_and_ai = SourceVeficication({})
+    verification_null_and_ai = SourceVerification({})
     verification_null_and_ai.column_compatibility("test_table", "test_column", version_null_and_ai, "createtable")
 
     assert verification_null_and_ai.issues == [("CRITICAL", "Unknown version -> createtable -> table:test_table -> column:test_column: Column attributes NULL and AUTO_INCREMENT are incompatible")]
@@ -86,7 +99,7 @@ def test_column_incompatibility():
         "a_i": True
     }
 
-    verification_json_and_ai = SourceVeficication({})
+    verification_json_and_ai = SourceVerification({})
     verification_json_and_ai.column_compatibility("test_table", "test_column", type_json_and_ai, "createtable")
 
     assert verification_json_and_ai.issues == [("CRITICAL", "Unknown version -> createtable -> table:test_table -> column:test_column: Column type JSON is incompatible with attribute AUTO_INCREMENT")]
@@ -96,7 +109,7 @@ def test_column_incompatibility():
         "unique": True
     }
 
-    verification_json_and_unique = SourceVeficication({})
+    verification_json_and_unique = SourceVerification({})
     verification_json_and_unique.column_compatibility("test_table", "test_column", type_json_and_unique, "createtable")
 
     assert verification_json_and_unique.issues == [("CRITICAL", "Unknown version -> createtable -> table:test_table -> column:test_column: Column type JSON is incompatible with attribute UNIQUE")]
