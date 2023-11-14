597abe8ba5cb0bcdb30b8543ee2eafb3929b2089
Wibo Kuipers <w.kuipers@filmage.nl>
2023-11-14 12:02:07 +0100

    Changed: Finalized single rename colomn concatenate test

--- tests/concatenate/column.py
+++ tests/concatenate/column.py
@@ -36,65 +36,64 @@ def test_concatenate_remove_recreate():
 
     assert concatenate_column(versions, table_name="table", column_name="col") == result
 
-# def test_rename_single_column():
-#     versions = [
-#         {
-#             "_id": "0.0.1",
-#             "createtable": {
-#                 "table": {
-#                     "col": {
-#                         "type": "VARCHAR",
-#                         "length": 200
-#                     }
-#                 }
-#             }
-#         },
-#         {
-#             "_id": "0.0.2",
-#             "altertable": {
-#                 "table": {
-#                     "renamecolumn": {
-#                         "col": "renamed"
-#                     }
-#                 }
-#             }
-#         },
-#         {
-#             "_id": "0.0.3",
-#             "altertable": {
-#                 "table": {
-#                     "modifycolumn": {
-#                         "col": {
-#                             "recreate": False,
-#                             "unique": True
-#                         }
-#                     }
-#                 }
-#             }
-#         },
-#         {
-#             "_id": "0.0.4",
-#             "altertable": {
-#                 "table": {
-#                     "modifycolumn": {
-#                         "renamed": {
-#                             "recreate": False,
-#                             "null": True
-#                         }
-#                     }
-#                 }
-#             }
-#         }
-#     ]
-#
-#     result = {
-#         "type": "VARCHAR",
-#         "length": 200,
-#         "null": True,
-#         "unique": True
-#     }
-#
-#     assert concatenate_column(versions, table_name="table", column_name="renamed") == result
+def test_rename_single_column():
+    versions = [
+        {
+            "_id": "0.0.1",
+            "createtable": {
+                "table": {
+                    "col": {
+                        "type": "VARCHAR",
+                        "length": 200
+                    }
+                }
+            }
+        },
+        {
+            "_id": "0.0.2",
+            "altertable": {
+                "table": {
+                    "renamecolumn": {
+                        "col": "renamed"
+                    }
+                }
+            }
+        },
+        {
+            "_id": "0.0.3", ## Should be ignored because uses old column name
+            "altertable": {
+                "table": {
+                    "modifycolumn": {
+                        "col": {
+                            "recreate": False,
+                            "unique": True
+                        }
+                    }
+                }
+            }
+        },
+        {
+            "_id": "0.0.4",
+            "altertable": {
+                "table": {
+                    "modifycolumn": {
+                        "renamed": {
+                            "recreate": False,
+                            "null": True
+                        }
+                    }
+                }
+            }
+        }
+    ]
+
+    result = {
+        "type": "VARCHAR",
+        "length": 200,
+        "null": True,
+    }
+
+    assert concatenate_column(versions, table_name="table", column_name="renamed") == result
 
 def test_rename_multiply_columns():
     versions = [
