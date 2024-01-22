eea9986ea7befc4538662f920aeb41676c4bc2da
w-kuipers <wibokuip@gmail.com>
2024-01-22 15:52:40 +0100

    Fix darwin node build scripts

---
+++ scripts/build-scripts/node_darwin.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+if len(sys.argv) < 3:
+    raise Exception("Both a version and architecture must be specified")
+
+else:
+    if not sys.argv[1][0] == "v":
+        raise Exception("No valid version supplied.")
+
+    version = sys.argv[1]
+    architecture = sys.argv[2]
+
+os.system(f"pyinstaller src/node/wrapper.py --distpath dist/node --name pywrapper_darwin_{architecture}")
