3881d96d28b64ceb963de7b69c0bbf639726002b
Wibo Kuipers <wibokuip@gmail.com>
2024-05-14 21:30:32 +0200

    Add error messages

--- src/alphadb/src/utils/error_messages.rs
+++ src/alphadb/src/utils/error_messages.rs
@@ -14,3 +14,24 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 pub const DB_CONFIG_NO_VERSION: &str = "There seems to be an issue with the database config. It is initialized, but does not return a valid version. Please manually check the configuration table in your database.";
+
+fn error(msg: String) {
+    panic!("{msg}");
+}
+
+/// Incomplete version object
+///
+/// - key: Missing object key
+/// - version: Location in version source (Version 1.0.0->table_name->column_name)
+pub fn incomplete_version_object(key: String, version: String) {
+    error(format!("Database version is incomplete or broken. {version} is missing key '{key}'.")); 
+}
+
+/// Incompatible column attributes
+///
+/// - attribute1: The incompatible MySQL column attribute
+/// - attribute2: The incompatible MySQL column attribute
+/// - version: Location in version source (Version 1.0.0->table_name->column_name)
+pub fn incompatible_column_attributes(attribute1: String, attribute2: String, version: String) {
+    error(format!("{version}: Column attributes {attribute1} and '{attribute2}' are not compatible.")); 
+}
