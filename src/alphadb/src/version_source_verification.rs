c1048d51dfd88ac12678c79219a1602dd15fa8aa
Wibo Kuipers <wibokuip@gmail.com>
2024-06-02 17:31:22 +0200

    Add column compatibility check to version source verification

--- src/alphadb/src/version_source_verification.rs
+++ src/alphadb/src/version_source_verification.rs
@@ -14,16 +14,16 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::utils::types::VerificationIssueLevel;
-use crate::utils::version_number::get_version_number_int;
+use crate::verification::compatibility::{INCOMPATIBLE_W_AI, INCOMPATIBLE_W_UNIQUE};
 use serde_json::Value;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct VerificationIssue {
     level: VerificationIssueLevel,
     message: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct VersionSourceVerification {
     version_source: Value,
     issues: Vec<VerificationIssue>,
@@ -57,7 +57,9 @@ impl VersionSourceVerification {
                 message: String::from("This version source does not contain any versions"),
             });
         } else {
-            for (i, version) in self.version_source["version"].as_array().unwrap().iter().enumerate() {
+            for (i, version) in self.version_source["version"].clone().as_array().unwrap().iter().enumerate() {
+                let mut version_output = format!("Version index {i}");
+
                 if !version.as_object().unwrap().keys().any(|k| k == "_id") {
                     self.issues.push(VerificationIssue {
                         level: VerificationIssueLevel::Critical,
@@ -71,12 +73,115 @@ impl VersionSourceVerification {
                             message: format!("{}: Version number is not convertable to an integer", version["_id"].as_str().unwrap()),
                         });
                     }
+                    version_output = format!("Version {}", version["_id"].as_str().unwrap());
+                }
+
+                for method in version.as_object().unwrap().keys() {
+                    match method.as_str() {
+                        "_id" => continue,
+                        "createtable" => self.createtable(version["createtable"].clone(), version_output.clone()),
+                        "altertable" => self.altertable(version["altertable"].clone(), version_output.clone()),
+                        _ => {
+                            self.issues.push(VerificationIssue {
+                                level: VerificationIssueLevel::High,
+                                message: format!("{version_output}: Method '{method}' does not exist"),
+                            });
+                        }
+                    }
                 }
             }
         }
 
-        println!("{:?}", self.issues);
+        if self.issues.is_empty() {
+            return Ok(true);
+        } else {
+            return Err(self.issues.clone());
+        }
+    }
 
-        return Ok(true);
+    /// **Createtable**
+    ///
+    /// Verify a single createtable block
+    pub fn createtable(&mut self, createtable: Value, version_output: String) {
+        if createtable.as_object().unwrap().is_empty() {
+            self.issues.push(VerificationIssue {
+                level: VerificationIssueLevel::Low,
+                message: format!("{version_output} -> createtable: Does not contain any data"),
+            });
+            return;
+        }
+
+        for table in createtable.as_object().unwrap().keys() {
+            for column in createtable[table].as_object().unwrap().keys() {
+                if column == "primary_key" {
+                    if !createtable[table]
+                        .as_object()
+                        .unwrap()
+                        .keys()
+                        .any(|p| p == createtable[table]["primary_key"].as_str().unwrap())
+                    {
+                        self.issues.push(VerificationIssue {
+                            level: VerificationIssueLevel::Critical,
+                            message: format!("{version_output} -> createtable -> table:{table}: Primary key does not match any column name"),
+                        });
+                    }
+                    continue;
+                }
+
+                self.column_compatibility(table, column, createtable[table][column].clone(), "createtable", version_output.clone());
+            }
+        }
+    }
+
+    /// **Altertable**
+    ///
+    /// Verify a single createtable block
+    pub fn altertable(&mut self, createtable: Value, version_output: String) {}
+
+    /// **Column compatibility**
+    ///
+    /// Verify column compatibility
+    pub fn column_compatibility(&mut self, table_name: &str, column_name: &str, data: Value, method: &str, version_output: String) {
+        let data_keys = data.as_object().unwrap().keys().into_iter().collect::<Vec<&String>>();
+
+        // NULL and AUTO_INCREMENT
+        if data_keys.contains(&&String::from("null")) && data_keys.contains(&&String::from("a_i")) {
+            self.issues.push(VerificationIssue {
+                level: VerificationIssueLevel::Critical,
+                message: format!("{version_output} -> {method} -> table:{table_name} -> column:{column_name}: Column attributes NULL and AUTO_INCREMENT are incompatible"),
+            });
+        }
+
+        // If type is defined
+        if !data_keys.contains(&&String::from("type")) {
+            if !data_keys.contains(&&String::from("recreate")) || data["recreate"].as_bool().unwrap() == true {
+                self.issues.push(VerificationIssue {
+                    level: VerificationIssueLevel::Critical,
+                    message: format!("{version_output} -> {method} -> table:{table_name} -> column:{column_name}: Does not contain a column type"),
+                });
+            }
+        } else {
+            // Types incompatible with auto increment
+            if INCOMPATIBLE_W_AI.contains(&&data["type"].as_str().unwrap().to_lowercase().as_str()) && data_keys.contains(&&String::from("a_i")) {
+                self.issues.push(VerificationIssue {
+                    level: VerificationIssueLevel::Critical,
+                    message: format!(
+                        "{version_output} -> {method} -> table:{table_name} -> column:{column_name}: Column type {} is incompatible with attribute AUTO_INCREMENT",
+                        data["type"].as_str().unwrap()
+                    ),
+                });
+            }
+
+            // Types incompatible with auto increment
+            if INCOMPATIBLE_W_UNIQUE.contains(&&data["type"].as_str().unwrap().to_lowercase().as_str()) && data_keys.contains(&&String::from("unique")) {
+                self.issues.push(VerificationIssue {
+                    level: VerificationIssueLevel::Critical,
+                    message: format!(
+                        "{version_output} -> {method} -> table:{table_name} -> column:{column_name}: Column type {} is incompatible with attribute UNIQUE",
+                        data["type"].as_str().unwrap()
+                    ),
+                });
+            }
+        }
     }
 }
