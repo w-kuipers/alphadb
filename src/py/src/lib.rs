dd9f979be2163aa45f566871af3eae4a6bec5357
Wibo Kuipers <wibokuip@gmail.com>
2024-08-04 11:25:49 +0200

    Change lib

--- src/py/src/lib.rs
+++ src/py/src/lib.rs
@@ -1,20 +1,53 @@
-use alphadb::AlphaDB as AlphaDBCore;
-use pyo3::prelude::*;
+use alphadb::{AlphaDB as AlphaDBCore};
+use pyo3::{prelude::*, types::PyDict, Python};
+use pyo3::types::IntoPyDict;
 
 #[pyclass]
-struct AlphaDBPythonWrapper(AlphaDBCore);
+struct AlphaDB {
+    alphadb_instance: AlphaDBCore,
+}
+
+#[pyclass]
+pub struct Check {
+    pub check: bool,
+    pub version: Option<String>,
+}
 
 #[pymethods]
-impl AlphaDBPythonWrapper {
+impl AlphaDB {
     #[new]
-    fn __new__(obj: &PyRawObject) -> PyResult<()> {
-        obj.init(|_token| AlphaDBPythonWrapper(AlphaDBCore {..})
+    fn __new__() -> Self {
+        Self {
+            alphadb_instance: AlphaDBCore::new(),
+        }
+    }
+
+    #[pyo3(signature = (host, user, password, database, port=3306))]
+    fn connect(
+        &mut self,
+        host: String,
+        user: String,
+        password: String,
+        database: String,
+        port: i32,
+    ) {
+        self.alphadb_instance
+            .connect(host, user, password, database, port)
+    }
+
+    // let key_vals: &[(&str, PyObject)] = [ ("num", 8.to_object()), ("str", "asd".to_object()) ]
+    // let dict = key_vals.into_py_dict(py);
+
+    fn check<'a>(&'a mut self, py: &'a Python) -> Bound<PyDict> {
+        let check = self.alphadb_instance.check();
+        let key_val: Vec<(&str, PyObject)> = vec![("check", check.check.to_object(py)), ("version", check.version.to_object(py))];
+
+        return key_val.into_py_dict_bound(*py);
     }
 }
 
-/// A Python module implemented in Rust.
-#[pymodule]
-fn alphadb_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+#[pymodule(name = "alphadb")]
+fn alphadb_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<AlphaDB>()?;
     Ok(())
 }
