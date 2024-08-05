d9ab1e053b4e7fc3dff616328edb6b1c49c7ae08
Wibo Kuipers <wibokuip@gmail.com>
2024-08-05 19:34:09 +0200

    Add check method to python wrapper

--- src/py/src/lib.rs
+++ src/py/src/lib.rs
@@ -1,6 +1,6 @@
-use alphadb::{AlphaDB as AlphaDBCore};
-use pyo3::{prelude::*, types::PyDict, Python};
-use pyo3::types::IntoPyDict;
+use alphadb::AlphaDB as AlphaDBCore;
+use pyo3::{prelude::*, Python};
+use std::collections::HashMap;
 
 #[pyclass]
 struct AlphaDB {
@@ -35,14 +35,16 @@ impl AlphaDB {
             .connect(host, user, password, database, port)
     }
 
-    // let key_vals: &[(&str, PyObject)] = [ ("num", 8.to_object()), ("str", "asd".to_object()) ]
-    // let dict = key_vals.into_py_dict(py);
+    fn check<'a>(&mut self) -> Py<PyAny> {
+        return Python::with_gil(|py: Python| {
+            let check = self.alphadb_instance.check();
+            let check_value = HashMap::from([
+                ("check", check.check.to_object(py)),
+                ("version", check.version.to_object(py)),
+            ]);
 
-    fn check<'a>(&'a mut self, py: &'a Python) -> Bound<PyDict> {
-        let check = self.alphadb_instance.check();
-        let key_val: Vec<(&str, PyObject)> = vec![("check", check.check.to_object(py)), ("version", check.version.to_object(py))];
-
-        return key_val.into_py_dict_bound(*py);
+            check_value.to_object(py)
+        });
     }
 }
 
