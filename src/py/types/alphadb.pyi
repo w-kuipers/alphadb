c9c977fb5611e3b4fdd869cb2b1a6677226e3214
Wibo Kuipers <wibokuip@gmail.com>
2024-10-07 21:25:04 +0200

    Added python types

--- src/py/types/alphadb.pyi
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-class Check(TypedDict):
-    check: bool
-    version: str
-
-class AlphaDB:
-    def check(self) -> Check: ...
-    """Check current database status"""
