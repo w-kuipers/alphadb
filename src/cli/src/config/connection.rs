9dd956cc0a7005c1fb281023f224cee209c0cbcf
Wibo Kuipers <wibokuip@gmail.com>
2024-10-23 20:36:55 +0200

    Add vacate command

--- src/cli/src/config/connection.rs
+++ src/cli/src/config/connection.rs
@@ -48,7 +48,6 @@ pub fn new_connection(activate: bool, config: &Config) -> String {
         .unwrap();
 
     let password = Password::new("Password")
-        .with_custom_confirmation_error_message("Passwords do not match")
         .without_confirmation()
         .with_validator(required!("This field is required"))
         .prompt()
