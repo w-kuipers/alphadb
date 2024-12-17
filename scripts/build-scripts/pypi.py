fc91851e02d12335865ae1b421792b6c67aa0441
Wibo Kuipers <wibokuip@gmail.com>
2024-12-17 21:07:19 +0100

    Fix

--- scripts/build-scripts/pypi.py
+++ scripts/build-scripts/pypi.py
@@ -19,29 +19,25 @@ with open(setup_path, "r") as i:
         i2.write(c.replace('"indev"', f'"{version}"'))
 
 # os.system("maturin build --release")
-# if sys.platform == "linux" or sys.platform == "linux2":
-#     docker_command = [
-#         "docker",
-#         "run",
-#         "--rm",
-#         "-v",
-#         "./:/io",  # Replace with your local project path
-#         "quay.io/pypa/manylinux2014_x86_64",
-#         "/bin/bash",
-#         "-c",
-#         """
-#             /opt/python/cp310-cp310/bin/pip install maturin && \
-#             /opt/python/cp310-cp310/bin/maturin build --release --manylinux 2014
-#             """,
-#     ]
-#     subprocess.Popen(docker_command, cwd=os.path.join(os.getcwd(), src_path)).wait()
-#
-# else:
-
-print(sys.platform)
 
 if sys.platform == "linux" or sys.platform == "linux2":
-    subprocess.Popen(["maturin", "build", "--release"], cwd=os.path.join(os.getcwd(), src_path)).wait()
+    docker_command = [
+        "docker",
+        "run",
+        "--rm",
+        "-v",
+        "./:/io",  # Replace with your local project path
+        "quay.io/pypa/manylinux2014_x86_64",
+        "/bin/bash",
+        "-c",
+        """
+            /opt/python/cp310-cp310/bin/pip install maturin && \
+            /opt/python/cp310-cp310/bin/maturin build --release --manylinux 2014
+        """,
+    ]
+    subprocess.Popen(docker_command, cwd=os.path.join(os.getcwd(), src_path)).wait()
+
+# subprocess.Popen(["maturin", "build", "--release"], cwd=os.path.join(os.getcwd(), src_path)).wait()
 else:
     subprocess.Popen(["maturin", "build", "--release", "--manylinux", "off"], cwd=os.path.join(os.getcwd(), src_path)).wait()
 
