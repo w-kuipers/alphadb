5f673df57cefd95a2b29f1a57f42dcf717d92a96
Wibo Kuipers <wibokuip@gmail.com>
2024-12-18 22:46:15 +0100

    Add node build script

---
+++ scripts/build-scripts/node.py
@@ -0,0 +1,88 @@
+import os
+import platform
+import shutil
+import subprocess
+import sys
+
+if len(sys.argv) == 1 or not sys.argv[1][0] == "v":
+    print("No valid version supplied.")
+    version = "dev"
+    if not input("Would you like to use version 'dev'? (y/N): ") == "y":
+        exit()
+else:
+    version = sys.argv[1]
+
+base_dir = "src/node"
+package_path = os.path.join(os.getcwd(), base_dir, "package.json")
+adb_path = os.path.join(os.getcwd(), "src/alphadb", "Cargo.toml")
+node_path = os.path.join(os.getcwd(), base_dir, "crates/alphadb/", "Cargo.toml")
+setup_paths = [adb_path, node_path]
+node_bin_dir = os.path.join(base_dir, "node-bin")
+
+os.mkdir(node_bin_dir)
+
+new_version_line = f'"version": "{version[1:]}",\n'
+
+with open(package_path, "r") as file:
+    lines = file.readlines()
+
+for i, line in enumerate(lines):
+    if '"version": "' in line:
+        lines[i] = new_version_line
+
+with open(package_path, "w") as file:
+    file.writelines(lines)
+
+new_version_line = f'version = "{version[1:]}"\n'
+node_version_line = f'version = "{version[1:]}-node"\n'
+
+for path in setup_paths:
+    with open(path, "r") as file:
+        lines = file.readlines()
+
+    for i, line in enumerate(lines):
+        if line.startswith("version ="):
+
+            if path == node_path:
+                lines[i] = node_version_line
+            else:
+                lines[i] = new_version_line
+
+    with open(path, "w") as file:
+        file.writelines(lines)
+
+mac = ["darwin-x64", "darwin-arm64"]
+win = ["win32-x64-msvc"]
+linux = ["linux-x64-gnu", "linux-arm64-gnu"]
+
+mac_r = ["x86_64-apple-darwin", "aarch64-apple-darwin"]
+win_r = ["x86_64-pc-windows-msvc"]
+linux_r = ["x86_64-unknown-linux-gnu", "aarch64-unknown-linux-gnu"]
+
+cwd = os.path.abspath(os.path.join(os.getcwd(), base_dir))
+
+subprocess.Popen(["yarn"], cwd=cwd, shell=True).wait()
+
+if sys.platform == "linux" or sys.platform == "linux2":
+    subprocess.Popen(["sudo", "apt", "install", "-y", "gcc-aarch64-linux-gnu"]).wait()
+    subprocess.Popen(["sudo", "apt", "install", "-y", "pkg-config", "libssl-dev"]).wait()
+    subprocess.Popen(["echo", '"OPENSSL_DIR=/usr/lib/ssl"', ">>", "$GITHUB_ENV"]).wait()
+    for i, system in enumerate(linux_r):
+        subprocess.Popen(["rustup", "target", "add", system], cwd=cwd).wait()
+        subprocess.Popen(["yarn", "build", "--target", system], cwd=cwd).wait()
+
+        shutil.move("src/node/index.node", os.path.join(node_bin_dir, f"{linux[i]}.node"))
+
+if sys.platform == "darwin":
+    for i, system in enumerate(mac_r):
+        subprocess.Popen(["rustup", "target", "add", system], cwd=cwd).wait()
+        subprocess.Popen(["yarn", "build", "--target", system], cwd=cwd).wait()
+
+        shutil.move("src/node/index.node", os.path.join(node_bin_dir, f"{mac[i]}.node"))
+
+if sys.platform == "win32":
+    for i, system in enumerate(win_r):
+        subprocess.Popen(["rustup", "target", "add", system], cwd=cwd, shell=True).wait()
+        subprocess.Popen(["yarn", "build", "--target", system], cwd=cwd, shell=True).wait()
+
+        shutil.move("src/node/index.node", os.path.join(node_bin_dir, f"{win[i]}.node"))
--- scripts/package-scripts/node.py
+++ scripts/package-scripts/node.py
@@ -1,38 +1,26 @@
 import os
 import shutil
 import sys
-
-if len(sys.argv) == 1 or not sys.argv[1][0] == "v":
-    raise Exception("No valid version supplied.")
-
-if not sys.argv[1][0] == "v":
-    raise Exception("No valid version supplied.")
-
-version = sys.argv[1]
-
-if not os.path.isdir("temp/node"):
-    os.makedirs("temp/node")
+from os.path import join
 
 #### Add all binaries (should also be defined in package.json)
 archs = ["linux_x86_64", "win32_x86_64", "darwin_x86_64"]
+cwd = os.getcwd()
+base_dir = join(cwd, "src/node")
+new_dir = join(cwd, "node-dist/")
 
-for arch in archs:
-    if os.path.isdir(f"dist/pywrapper_{arch}"):
-        shutil.move(f"dist/pywrapper_{arch}", f"temp/node/pywrapper_{arch}")
-        if "linux" in arch or "darwin" in arch:
-            os.system(f"cd temp/node/pywrapper_{arch}; chmod +x pywrapper_{arch}")
+os.mkdir(new_dir)
 
-#### Change version number in package.json
-with open("src/node/package.json", "r") as fr:
-    version_updated = fr.read().replace('"indev"', f'"{version[1:]}"')
-    with open("temp/node/package.json", "w") as fw:
-        fw.write(version_updated)
 
-shutil.copy("src/node/index.ts", "temp/node/index.ts")
-shutil.copy("src/node/alphadb.ts", "temp/node/alphadb.ts")
-shutil.copy("src/node/tsconfig.json", "temp/node/tsconfig.json")
-shutil.copy("LICENSE", "temp/node/LICENSE")
-shutil.copy("src/node/.npmignore", "temp/node/.npmignore")
-shutil.copy("readme/npm.md", "temp/node/README.md")
+def mv(file):
+    shutil.copy(join(base_dir, file), join(new_dir, file))
 
-os.system("cd temp/node; yarn; yarn build")
+
+def mvd(directory):
+    shutil.copytree(join(base_dir, directory), join(new_dir, directory))
+
+
+mv("package.json")
+mv("LICENSE")
+mv("README.md")
+mvd("lib")
