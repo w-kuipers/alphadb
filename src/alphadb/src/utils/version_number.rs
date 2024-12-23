eeae5b39be78c310938b8a61d05ce20f67167225
Wibo Kuipers <wibokuip@gmail.com>
2024-12-23 22:31:20 +0100

    Change get_version_number_int to parse_version_number

--- src/alphadb/src/utils/version_number.rs
+++ src/alphadb/src/utils/version_number.rs
@@ -13,36 +13,67 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use crate::utils::errors::{AlphaDBError, Get};
+use std::num::ParseIntError;
+use thiserror::Error;
 
 /// Validate if a string can be used as a version number.
 /// This will return true when the string can be converted to an integer. Any dots will be
 /// stripped.
 ///
 /// version_number: The version number to validate
-pub fn validate_version_number(version_number: &str) -> Result<bool, std::num::ParseIntError> {
+pub fn validate_version_number(version_number: &str) -> Result<bool, ParseIntError> {
     let version_number = version_number.replace(".", "");
     version_number.parse::<i32>()?;
 
     return Ok(true);
 }
 
-pub fn get_version_number_int(version_number: &String) -> u32 {
+/// Parse the version number to an integer
+pub fn parse_version_number(version_number: &str) -> Result<u32, ParseIntError> {
     let version_number = version_number.replace(".", "");
-    version_number.parse::<u32>().expect("Could not convert version to integer")
+    Ok(version_number.parse::<u32>()?)
+}
+
+#[derive(Error, Debug)]
+pub enum LatestVersionError {
+    #[error(transparent)]
+    AlphaDbError(#[from] AlphaDBError),
+
+    #[error(transparent)]
+    ParseIntError(#[from] ParseIntError),
+}
+
+impl Get for LatestVersionError {
+    fn message(&self) -> String {
+        match self {
+            LatestVersionError::AlphaDbError(e) => e.message(),
+            LatestVersionError::ParseIntError(e) => format!("ParseIntError: {:?}", e),
+        }
+    }
+    fn error(&self) -> String {
+        match self {
+            LatestVersionError::AlphaDbError(e) => e.error(),
+            LatestVersionError::ParseIntError(_) => String::from(""),
+        }
+    }
 }
 
 /// Get the latest version in a version source
 ///
 /// versions: Vector of versions from version source
-pub fn get_latest_version(versions: &Vec<serde_json::Value>) -> String {
-    let mut latest_version = String::from("0.0.0");
+pub fn get_latest_version(versions: &Vec<serde_json::Value>) -> Result<String, LatestVersionError> {
+    let mut latest_version = "0.0.0";
     for version in versions {
-        let version = version["_id"].as_str().expect("No verssion number was specified");
+        let version = version["_id"].as_str().ok_or(AlphaDBError {
+            message: format!("No version number specified"),
+            ..Default::default()
+        })?;
 
-        if get_version_number_int(&String::from(version)) > get_version_number_int(&latest_version) {
-            latest_version = version.to_string();
+        if parse_version_number(version)? > parse_version_number(latest_version)? {
+            latest_version = version;
         }
     }
 
-    latest_version
+    Ok(latest_version.to_string()) // Maybe just &str?
 }
