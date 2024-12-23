ce3cab327c79b9157f155de06b1a2aad45f41119
Wibo Kuipers <wibokuip@gmail.com>
2024-12-23 21:35:40 +0100

    Add better error to verify_version_number

--- src/alphadb/src/utils/version_number.rs
+++ src/alphadb/src/utils/version_number.rs
@@ -1,7 +1,29 @@
-pub fn verify_version_number(version_number: &String) -> bool {
+// Copyright (C) 2024 Wibo Kuipers
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+/// Verify if a string can be used as a version number.
+/// This will return true when the string can be converted to an integer. Any dots will be
+/// stripped.
+///
+/// version_number: The version number to verify
+pub fn verify_version_number(version_number: &String) -> Result<bool, std::num::ParseIntError> {
     let version_number = version_number.replace(".", "");
-    version_number.parse::<i32>().expect("Could not convert version to integer");
-    return true;
+    version_number.parse::<i32>()?;
+
+    return Ok(true);
 }
 
 pub fn get_version_number_int(version_number: &String) -> u32 {
