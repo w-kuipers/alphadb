a488ae9cbdfa9726a3bee88945e71a79f1429618
Wibo Kuipers <wibokuip@gmail.com>
2024-12-28 13:53:13 +0100

    Add return types to consolidate column functions

--- src/alphadb/src/utils/consolidate/column.rs
+++ src/alphadb/src/utils/consolidate/column.rs
@@ -36,7 +36,7 @@ pub struct RenameData {
 pub fn consolidate_column(version_list: &Value, column_name: &str, table_name: &str) -> Result<Value, AlphaDBError> {
     let mut column = json!({});
     let mut version_column_name = column_name;
-    let rename_data = get_column_renames(version_list, column_name, table_name, "DESC");
+    let rename_data = get_column_renames(version_list, column_name, table_name, "DESC")?;
     let version_list_cloned = version_list.clone();
 
     for version in version_list_cloned.as_array().unwrap() {
@@ -125,10 +125,10 @@ pub fn consolidate_column(version_list: &Value, column_name: &str, table_name: &
 /// - column_name: Name of the column to be handled
 /// - table_name: Name of the table the column is in
 /// - order: The order in which to walk over the version source. Either "ASC" or "DESC".
-pub fn get_column_renames(version_list: &Value, column_name: &str, table_name: &str, order: &str) -> Vec<RenameData> {
+pub fn get_column_renames(version_list: &Value, column_name: &str, table_name: &str, order: &str) -> Result<Vec<RenameData>, AlphaDBError> {
     let mut rename_data: Vec<RenameData> = Vec::new();
 
-    let mut version_loop = |version: &Value| {
+    let mut version_loop = |version: &Value| -> Result<bool, AlphaDBError> {
         if version.as_object().unwrap().keys().any(|i| i == "altertable") {
             if version["altertable"].as_object().unwrap().keys().any(|t| t == table_name) {
                 let v = parse_version_number(get_json_string(&version["_id"]).unwrap()).unwrap();
@@ -136,12 +136,12 @@ pub fn get_column_renames(version_list: &Value, column_name: &str, table_name: &
                 // Skip version that are already processed
                 if order == "DESC" {
                     if rename_data.iter().any(|r| r.rename_version <= v) {
-                        return false;
+                        return Ok(false);
                     }
                 }
                 if order == "ASC" {
                     if rename_data.iter().any(|r| r.rename_version >= v) {
-                        return false;
+                        return Ok(false);
                     }
                 }
 
@@ -150,14 +150,14 @@ pub fn get_column_renames(version_list: &Value, column_name: &str, table_name: &
 
                     // If the current column is not the one being renamed, continue
                     if order == "DESC" && !renamecolumn_values.iter().any(|&k| k == column_name) {
-                        return false;
+                        return Ok(false);
                     }
 
                     let renamecolumn_keys = version["altertable"][table_name]["renamecolumn"].as_object().unwrap().keys().collect::<Vec<&String>>();
 
                     // If the current column is not the one being renamed, continue
                     if order == "ASC" && !renamecolumn_keys.iter().any(|&k| k == column_name) {
-                        return false;
+                        return Ok(false);
                     }
 
                     // Get old or new name based on order
@@ -185,24 +185,24 @@ pub fn get_column_renames(version_list: &Value, column_name: &str, table_name: &
                     }
 
                     // Recursively call it again with new column name
-                    rename_data.append(&mut get_column_renames(version_list, name, table_name, order));
-                    return true; // Return true to break the loop as the current column name does not exist
+                    rename_data.append(&mut get_column_renames(version_list, name, table_name, order)?);
+                    return Ok(true); // Return true to break the loop as the current column name does not exist
                 }
             }
         }
 
-        return false;
+        return Ok(false);
     };
 
     if order == "ASC" {
         for version in version_list.as_array().unwrap().into_iter() {
-            if version_loop(version) {
+            if version_loop(version)? {
                 break;
             }
         }
     } else if order == "DESC" {
         for version in version_list.as_array().unwrap().into_iter().rev() {
-            if version_loop(version) {
+            if version_loop(version)? {
                 break;
             }
         }
@@ -210,7 +210,7 @@ pub fn get_column_renames(version_list: &Value, column_name: &str, table_name: &
         error("Order in function 'get_column_renames' must be either 'ASC' or 'DESC'.".to_string());
     }
 
-    return rename_data;
+    return Ok(rename_data);
 }
 
 #[cfg(test)]
@@ -327,7 +327,7 @@ mod get_column_renames_tests {
         ]);
 
         assert_eq!(
-            get_column_renames(&versions, "multiplerenamed", "table", "DESC"),
+            get_column_renames(&versions, "multiplerenamed", "table", "DESC").unwrap(),
             [
                 RenameData {
                     new_name: "multiplerenamed".to_string(),
@@ -362,7 +362,7 @@ mod get_column_renames_tests {
         ]);
 
         assert_eq!(
-            get_column_renames(&versions, "col", "table", "ASC"),
+            get_column_renames(&versions, "col", "table", "ASC").unwrap(),
             [
                 RenameData {
                     new_name: "renamed".to_string(),
