034c6706b1d8cd43f4ad0a23b0be3346310eb88a
Wibo Kuipers <wibokuip@gmail.com>
2025-06-28 16:58:36 +0200

    Handle verification for default data

--- src/alphadb/src/verification/compatibility.rs
+++ src/alphadb/src/verification/compatibility.rs
@@ -13,16 +13,26 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub const SUPPORTED_COLUMN_TYPES: [&str; 10] = [
-    "INT", "TINYINT", "BIGINT", "TEXT", "LONGTEXT", "FLOAT", "DECIMAL", "VARCHAR", "DATETIME",
-    "JSON",
-];
+/// All columns supported by AlphaDB
+pub const SUPPORTED_COLUMN_TYPES: [&str; 10] = ["INT", "TINYINT", "BIGINT", "TEXT", "LONGTEXT", "FLOAT", "DECIMAL", "VARCHAR", "DATETIME", "JSON"];
 
-pub const INCOMPATIBLE_W_AI: [&str; 6] =
-    ["varchar", "text", "longtext", "datetime", "decimal", "json"];
+/// All column types that should take a string value as inserted data
+pub const FLOAT_COLUMNS: [&str; 2] = ["FLOAT", "DECIMAL"];
 
+/// All column types that should take a string value as inserted data
+pub const INT_COLUMNS: [&str; 4] = ["INT", "TINYINT", "BIGINT", "DATETIME"];
+
+/// All column types that should take a string value as inserted data
+pub const STRING_COLUMNS: [&str; 4] = ["TEXT", "LONGTEXT", "VARCHAR", "DATETIME"];
+
+/// All column types that are incompatible with the AUTO_INCREMENT setting
+pub const INCOMPATIBLE_W_AI: [&str; 6] = ["varchar", "text", "longtext", "datetime", "decimal", "json"];
+
+/// All column types that are incompatible with the UNIQUE key
 pub const INCOMPATIBLE_W_UNIQUE: [&str; 1] = ["json"];
 
 /// All the MySQL column types that allow a decimal length value
 pub const ALLOW_DECIMAL_LENGTH: [&str; 3] = ["decimal", "float", "double"];
 
+/// All the version source table keys that do not represent a column
+pub const NON_COLUMN_TABLE_KEYS: [&str; 1] = ["primary_key"];
